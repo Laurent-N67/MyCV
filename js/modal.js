90e701bc215c2d41c77ec3198ebf3da47d6080b1
Laurent-N67 <laurent.nie67@gmail.com>
2022-10-18 16:00:36 +0100

    Mobile L done

--- js/modal.js
+++ js/modal.js
@@ -23,11 +23,11 @@ btn2.onclick = function() {
 span2.onclick = function() {
   modal2.style.display = "none";
 }
-window.onclick = function(event) {
+window.addEventListener("click", function(event){
   if (event.target == modal2) {
     modal2.style.display = "none";
   }
-}
+})
 
 var modal3 = document.getElementById("modal3");
 var btn3 = document.getElementById("btn3");
@@ -178,4 +178,171 @@ window.addEventListener("click", function(event) {
     modal12.style.display = "none";
   }
 })
-
+var modal13 = document.getElementById("modal13");
+var btn13 = document.getElementById("btn13");
+var span13 = document.getElementsByClassName("close")[12];
+btn13.onclick = function() {
+  modal13.style.display = "block";
+}
+span13.onclick = function() {
+  modal13.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal13) {
+    modal13.style.display = "none";
+  }
+})
+var modal14 = document.getElementById("modal14");
+var btn14 = document.getElementById("btn14");
+var span14 = document.getElementsByClassName("close")[13];
+btn14.onclick = function() {
+  modal14.style.display = "block";
+}
+span14.onclick = function() {
+  modal14.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal14) {
+    modal14.style.display = "none";
+  }
+})
+var modal15 = document.getElementById("modal15");
+var btn15 = document.getElementById("btn15");
+var span15 = document.getElementsByClassName("close")[14];
+btn15.onclick = function() {
+  modal15.style.display = "block";
+}
+span15.onclick = function() {
+  modal15.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal15) {
+    modal15.style.display = "none";
+  }
+})
+var modal16 = document.getElementById("modal16");
+var btn16 = document.getElementById("btn16");
+var span16 = document.getElementsByClassName("close")[15];
+btn16.onclick = function() {
+  modal16.style.display = "block";
+}
+span16.onclick = function() {
+  modal16.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal16) {
+    modal16.style.display = "none";
+  }
+})
+var modal17 = document.getElementById("modal17");
+var btn17 = document.getElementById("btn17");
+var span17 = document.getElementsByClassName("close")[16];
+btn17.onclick = function() {
+  modal17.style.display = "block";
+}
+span17.onclick = function() {
+  modal17.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal17) {
+    modal17.style.display = "none";
+  }
+})
+var modal18 = document.getElementById("modal18");
+var btn18 = document.getElementById("btn18");
+var span18 = document.getElementsByClassName("close")[17];
+btn18.onclick = function() {
+  modal18.style.display = "block";
+}
+span18.onclick = function() {
+  modal18.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal18) {
+    modal18.style.display = "none";
+  }
+})
+var modal19 = document.getElementById("modal19");
+var btn19 = document.getElementById("btn19");
+var span19 = document.getElementsByClassName("close")[18];
+btn19.onclick = function() {
+  modal19.style.display = "block";
+}
+span19.onclick = function() {
+  modal19.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal19) {
+    modal19.style.display = "none";
+  }
+})
+var modal20 = document.getElementById("modal20");
+var btn20 = document.getElementById("btn20");
+var span20 = document.getElementsByClassName("close")[19];
+btn20.onclick = function() {
+  modal20.style.display = "block";
+}
+span20.onclick = function() {
+  modal20.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal20) {
+    modal20.style.display = "none";
+  }
+})
+var modal21 = document.getElementById("modal21");
+var btn21 = document.getElementById("btn21");
+var span21 = document.getElementsByClassName("close")[20];
+btn21.onclick = function() {
+  modal21.style.display = "block";
+}
+span21.onclick = function() {
+  modal21.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal21) {
+    modal21.style.display = "none";
+  }
+})
+var modal22 = document.getElementById("modal22");
+var btn22 = document.getElementById("btn22");
+var span22 = document.getElementsByClassName("close")[21];
+btn22.onclick = function() {
+  modal22.style.display = "block";
+}
+span22.onclick = function() {
+  modal22.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal22) {
+    modal22.style.display = "none";
+  }
+})
+var modal23 = document.getElementById("modal23");
+var btn23 = document.getElementById("btn23");
+var span23 = document.getElementsByClassName("close")[22];
+btn23.onclick = function() {
+  modal23.style.display = "block";
+}
+span23.onclick = function() {
+  modal23.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal23) {
+    modal23.style.display = "none";
+  }
+})
+var modal24 = document.getElementById("modal24");
+var btn24 = document.getElementById("btn24");
+var span24 = document.getElementsByClassName("close")[23];
+btn24.onclick = function() {
+  modal24.style.display = "block";
+}
+span24.onclick = function() {
+  modal24.style.display = "none";
+}
+window.addEventListener("click", function(event) {
+  if (event.target == modal24) {
+    modal24.style.display = "none";
+  }
+})
